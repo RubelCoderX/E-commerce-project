2cb83091c70f844cb393eb3d30547851a227d058
MD RUBEL <mdrubel29879@gmail.com>
2023-12-17 16:49:27 +0530

    other login option added

--- src/components/Pages/LoginSignup.jsx
+++ src/components/Pages/LoginSignup.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CSS/LoginSignup.css';
+import {  FaGithub, FaGoogle } from "react-icons/fa";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from '../../firebase.init';
+
 
 const LoginSignup = () => {
+    const[user, setUser] = useState(null);
+    const auth = getAuth(app);
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
+    const handleGoogleSingIn = () =>{
+        signInWithPopup(auth, googleProvider)
+        .then(result =>{
+            const loggedUser = result.user;
+            setUser(loggedUser);
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+    }
+    
+    const handleGithubSignIn =() =>{
+        signInWithPopup(auth, githubProvider)
+        .then(result => {
+            const loggedUser = result.user;
+            setUser(loggedUser);
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+    }
     return (
         <div className='loginsignup'>
             <div className="loginsignup-container">
@@ -11,10 +41,18 @@ const LoginSignup = () => {
                     <input type="text" placeholder='Email Address' />
                     <input type="password" placeholder='Password'/>
                 </div>
-                <button>Continue</button>
+                <button className='continue'>Continue</button>
                 <p className="loginsignup-login">
-                   Already have an account? <span>Login here</span> 
-                   <div className="loginsignup-agree">
+                   Already have an account? <span>Login here</span>
+                   <hr /> 
+                   <p>or use one of these options</p>
+                   <hr />
+                   <div className='other-btn'>
+                      <button onClick={handleGoogleSingIn} className='google-icon'><FaGoogle  className='icon'/></button>
+                      <button onClick={handleGithubSignIn} className='github-icon'><FaGithub  className='icon'/></button>
+                   </div>
+                   
+                    <div className="loginsignup-agree">
                     <input type="checkbox" name="" id="" />
                     <p>By continuing,I agree to the terms of use & privacy policy</p>
                    </div>
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCu4FCcs-E3v_lmL9FTsMIlNQAbuGAJSGM",
+  authDomain: "e-commerce-project-926b9.firebaseapp.com",
+  projectId: "e-commerce-project-926b9",
+  storageBucket: "e-commerce-project-926b9.appspot.com",
+  messagingSenderId: "872224409859",
+  appId: "1:872224409859:web:8ae25d7bd5a80d3dc00136"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
